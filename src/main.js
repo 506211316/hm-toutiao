b44b00625defbad8f589f601b2bf2c757a475f7d
zhouxu <506211316@qq.com>
2019-08-23 17:29:17 +0800

    完善功能

--- src/main.js
+++ src/main.js
@@ -9,10 +9,18 @@ import 'element-ui/lib/theme-chalk/index.css'
 
 // 导入less文件
 import './styles/index.less'
+
+// 导入axios组件
+import axios from 'axios'
+
 // 导入路由组件
 import router from '@/router'
+
 // 注册element-ui
 Vue.use(ElementUi)
+
+// 把axios定义为Vue里面的一个成员
+Vue.prototype.$http = axios
 
 new Vue({
   router,
