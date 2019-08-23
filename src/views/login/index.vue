86b351719fc3fec7d83eefe30ba0ee4aa6b36960
zhouxu <506211316@qq.com>
2019-08-23 16:06:09 +0800

    登录提交功能第一部分

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,15 +5,22 @@
       <!-- 插入logo图片 -->
       <img src="../../assets/images/logo_index.png" alt />
       <!-- 插入form表单，设置控件 -->
-      <el-form :model="form" red="loginForm">
-        <el-form-item>
-          <el-input v-model="form.name" placeholder="请输入手机号"></el-input>
+      <el-form :model="loginForm" :rules="loginRules">
+        <!-- 输入手机号模块 -->
+        <el-form-item prop="mobile">
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
         </el-form-item>
         <!-- 输入验证码的模块 -->
-        <el-form-item>
-          <el-input v-model="form.message" placeholder="请输入验证码" style="width:236px; margin-right:10px"></el-input>
+        <el-form-item prop="code">
+          <el-input
+            v-model="loginForm.code"
+            placeholder="请输入验证码"
+            style="width:236px; margin-right:10px"
+          ></el-input>
+          <!-- 发送验证码按钮 -->
           <el-button>发送验证码</el-button>
         </el-form-item>
+        <!-- 同意条款 -->
         <el-form-item>
           <el-checkbox :value="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
         </el-form-item>
@@ -28,10 +35,26 @@
 <script>
 export default {
   data () {
+    const checkLogin = (rule, value, callback) => {
+      if (!/^1[3-9]\d{9}$/.test(value)) {
+        return callback(new Error('手机号格式不正确'))
+      }
+      callback()
+    }
     return {
-      form: {
-        name: '',
-        message: ''
+      loginForm: {
+        mobile: '',
+        code: ''
+      },
+      loginRules: {
+        mobile: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          { validator: checkLogin, trigger: 'change' }
+        ],
+        code: [
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+          { len: 6, message: '请输入6位数字', trigger: 'blur' }
+        ]
       }
     }
   },
