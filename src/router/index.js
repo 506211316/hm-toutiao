071abf777820f8989546b6d467c293ad31ca48f5
zhouxu <506211316@qq.com>
2019-08-23 20:24:12 +0800

    home页面的头部完成

--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,15 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 // 导入login组件
 import Login from '../views/login/index.vue'
+// 导入home主页面组件
+import Home from '../views/home/home.vue'
 // 注册路由
 Vue.use(VueRouter)
 // 实例化路由
 const router = new VueRouter({
   // 规定路由规则
   routes: [
+    { path: '/', name: 'home', component: Home },
     { path: '/login', name: 'login', component: Login }
   ]
 })
---
+++ src/views/home/home.vue
@@ -0,0 +1,59 @@
+<template>
+  <el-container class="my-container">
+    <el-aside width="200px">Aside</el-aside>
+    <el-container>
+      <!-- header内容 -->
+      <el-header>
+        <span class="icon el-icon-s-fold"></span>
+        <span class="headerText">江苏传智播客科技教育有限公司</span>
+        <!-- 下拉菜单 -->
+        <el-dropdown class="rightList">
+          <span class="el-dropdown-link">
+            <!-- 要显示的内容 -->
+            下拉菜单
+            <i class="el-icon-arrow-down el-icon--right"></i>
+          </span>
+          <!-- 下拉菜单的内容 -->
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>个人设置</el-dropdown-item>
+            <el-dropdown-item>退出登录</el-dropdown-item>
+          </el-dropdown-menu>
+          <!-- 下拉菜单的内容end -->
+        </el-dropdown>
+        <!-- 下拉菜单end -->
+      </el-header>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less" scoped>
+.my-container {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  .el-header {
+    border-bottom: 1px solid #ddd;
+    line-height: 60px;
+    .icon {
+      font-size: 24px;
+      vertical-align: middle;
+    }
+    .headerText {
+      vertical-align: middle;
+    }
+    .rightList {
+      float: right;
+    }
+  }
+  .el-aside {
+    background: #002033;
+  }
+}
+</style>
